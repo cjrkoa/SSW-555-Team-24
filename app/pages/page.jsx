c21442dd9f88227fa59a7c9191e74fbe411425dd
Samuelp0110 <spreston110@gmail.com>
2023-10-29 18:57:02 -0400

    Updated Navbar and Footer
    Fully revamped navbar and footer with ready to use href links. Also added some styles to global css

--- app/pages/page.jsx
+++ app/pages/page.jsx
@@ -1,25 +1,18 @@
 export default function Home() {
   return (
     <main className = "">
-      <div className = "flex items-baseline mt-4 mb-4 pb-6 border-b border-slate-200">
-        <div className = "w-2/5 h-96 bg-white shadow rounded">
-          Map Box
+      <div className="w-[1065px] h-[715px] justify-center items-center gap-[15px] inline-flex">
+        <div className="w-[615px] h-[350px] relative">
+          <div className="w-[615px] h-[350px] left-0 top-0 absolute bg-yellow-200 bg-opacity-80" />
+          <div className="w-[529px] h-[114px] left-[43px] top-[15px] absolute text-black text-[42px] font-semibold font-['Outfit'] capitalize tracking-widest">Looking for an event near you?</div>
         </div>
-        <div className = "w-2/5 min-w-max grid grid-cols-3 auto-rows-auto gap-2">
-          <span>01</span>
-          <span>02</span>
-          <span>03</span>
-          <span>04</span>
-          <span>05</span>
-          <span>06</span>
-          <span>06</span>
+        <img className="w-[435px] h-[350px] rounded-[40px]" src="https://via.placeholder.com/435x350" />
+        <div className="w-[615px] h-[350px] relative">
+          <div className="w-[615px] h-[350px] left-0 top-0 absolute bg-yellow-200 bg-opacity-80" />
+          <div className="w-[529px] h-[70px] left-[43px] top-[15px] absolute text-black text-[42px] font-semibold font-['Outfit'] capitalize tracking-widest">Hosting an Event?</div>
         </div>
-        <div className = "w-1/5 grid">
-          <input type="Range" className = "mt-4 mb-4 border" />
-          <input type="Date" />
-          <input type="Cost" />
-        </div>
-      </div>
+        <img className="w-[435px] h-[350px] rounded-[40px]" src="https://via.placeholder.com/435x350" />
+    </div>
     </main>
   )
 }
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,38 +1,46 @@
-const Footer = () => {
-  return (
-    <footer className="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
-        <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-                © 2023 
-                <a href="https://flowbite.com/" className="hover:underline">
-                Sharevent
-                </a>
-            </span>
-            <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
-                <li>
-                    <a href="#" className="mr-4 hover:underline md:mr-6 ">
-                        About
-                    </a>
-                </li>
-                <li>
-                    <a href="#" className="mr-4 hover:underline md:mr-6">
-                        Privacy Policy
-                    </a>
-                </li>
-                <li>
-                    <a href="#" className="mr-4 hover:underline md:mr-6">
-                        Licensing
-                    </a>
-                </li>
-                <li>
-                    <a href="#" className="hover:underline">
-                        Contact
-                    </a>
-                </li>
-            </ul>
-        </div>  
-    </footer>
-  )
-}
-
-export default Footer
+const LINKS = [
+    {
+      title: "Connect",
+      items: ["Help", "Inquire", "Liscensing"]
+    },
+    {
+      title: "Finding Events",
+      items: ["Recommendations", "Searching"]
+    },
+    {
+      title: "Creating Events",
+      items: ["Demonstration", "Limitations"]
+    }
+  ];
+  
+  const currentYear = new Date().getFullYear();
+  
+  const Footer = () => {
+    return (
+      <footer className="footer">
+        <div className="content">
+          <h5>SHAREVENT</h5>
+          <div className="links">
+            {LINKS.map(({ title, items }) => (
+              <ul key={title}>
+                <li className="title">{title}</li>
+                {items.map((link) => (
+                  <li key={link}>
+                    <a href="#" className="link">{link}</a>
+                  </li>
+                ))}
+              </ul>
+            ))}
+          </div>
+        </div>
+        <div className="footer-bottom">
+          <p>
+            &copy; {currentYear} <a href="#">Sharevent</a>. All Rights Reserved.
+          </p>
+          {/* Social icons can be added here */}
+        </div>
+      </footer>
+    );
+  }
+  
+  export default Footer

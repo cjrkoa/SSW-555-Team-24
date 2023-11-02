ff33c12d320ec316a468bd5ecaee3c6683b573f6
Christopher Roddy <76755988+cjrkoa@users.noreply.github.com>
2023-11-02 17:37:06 -0400

    AddEvent styling progress

--- app/createvent/page.jsx
+++ app/createvent/page.jsx
@@ -13,23 +13,39 @@ export default function AddEvent() {
     
     return(
         <Fragment>
-            <ui>
-                <h1>name</h1>
-                <input name="name" onChange={() => setName()} />
-                <h1>date</h1>
-                <input name="date" onChange={() => setDate()}/>
-                <h1>price</h1>
-                <input name="price" onChange={() => setPrice()}/>
-                <h1>category</h1>
-                <input name="category" onChange={() => setCategory()}/>
-                <h1>type</h1>
-                <input name="type" onChange={() => setType()}/>
-                <h1>language</h1>
-                <input name="language" onChange={() => setLangauge()}/>
-                <h1>currency</h1>
-                <input name="currency" onChange={() => setCurrency()}/>
+            <h1>Enter information to create a new event below</h1>
+            <div class="grid gap-1 grid-cols-1 grid-rows-1">
+                <div class="inline-flex">
+                    <h1>name</h1>
+                    <input name="name" onChange={() => setName()} />
+                </div>
+                <div class="inline-flex">
+                    <h1>date</h1>
+                    <input name="date" onChange={() => setDate()}/>
+                </div>
+                <div class="inline-flex">
+                    <h1>price</h1>
+                    <input name="price" onChange={() => setPrice()}/>
+                </div>
+                <div class="inline-flex">
+                    <h1>category</h1>
+                    <input name="category" onChange={() => setCategory()}/>
+                </div>
+                <div class="inline-flex">
+                    <h1>type</h1>
+                    <input name="type" onChange={() => setType()}/>
+                </div>
+                <div class="inline-flex">
+                    <h1>language</h1>
+                    <input name="language" onChange={() => setLangauge()}/>
+                </div>
+                <div class="inline-flex">
+                    <h1>currency</h1>
+                    <input name="currency" onChange={() => setCurrency()}/>
+                </div>
+                
                 <button onClick={() => console.log(name)}>Submit</button>
-            </ui>
+            </div>
         </Fragment>
     );
 }

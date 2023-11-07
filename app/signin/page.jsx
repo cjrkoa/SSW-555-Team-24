9522434637500a4d7e7fc6d08595b27930f1a81b
Frank Barton <frankcbarton@gmail.com>
2023-11-06 19:03:33 -0500

    Finish adding elements to signin and signup pages

--- app/signin/page.jsx
+++ app/signin/page.jsx
@@ -1,5 +1,23 @@
 import { Fragment } from "react";
+import './SignIn.css';
 
 export default function SignIn() {
-  return <Fragment>Hello World</Fragment>;
+  return (
+    <Fragment>
+      <div className="sign-in-page">
+        <div className="sign-in-container">
+          <h1 className="sign-in-heading">Sign In</h1>
+          <form className="sign-in-form">
+            <input type="email" placeholder="Email" required />
+            <input type="password" placeholder="Password" required />
+            <button type="submit">Sign In</button>
+          </form>
+        </div>
+        <div className="sign-up-container">
+          <h2 className="sign-up-question">Don't have an account?</h2>
+          <a href="/signup"><button>Create Account</button></a>
+        </div>
+      </div>
+    </Fragment>
+  );
 }
--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -1,10 +1,25 @@
-export default function signup() {
-    return (
-        <main>
-            <h1>Sign up below</h1>
-            <div>
-                <p></p>
-            </div>
-        </main>
-    )
+import { Fragment } from "react";
+import './SignUp.css';
+
+export default function SignIn() {
+  return (
+    <Fragment>
+      <div className="sign-up-page">
+        <div className="sign-up-container">
+          <h1 className="sign-up-heading">Create Account</h1>
+          <form className="sign-up-form">
+            <input type="email" placeholder="Email" required />
+            <input type="password" placeholder="Password" required />
+            <input type="First Name" placeholder="First Name" required />
+            <input type="Last Name" placeholder="Last Name" required />
+            <button type="submit">Sign Up</button>
+          </form>
+        </div>
+        <div className="sign-in-container">
+          <h2 className="sign-in-question">Already have an account?</h2>
+          <a href="/signin"><button>Sign In Here</button></a>
+        </div>
+      </div>
+    </Fragment>
+  );
 }

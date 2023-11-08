a13bb24faa92300ad4c0a90836c64aa5d21ffc8a
Samuelp0110 <spreston110@gmail.com>
2023-11-08 15:50:44 -0500

    Slight homepage format update

--- app/page.jsx
+++ app/page.jsx
@@ -5,24 +5,20 @@ import concert from './image/concert-stock.jpg'
 export default function Home() {
   return (
     <main className = "">
-      <div class="grid gap-20 grid-cols-3 grid-rows-2">
-        <div class="col-span-2">
-          <div className="image-container">
-            <h2>Looking to find an event?</h2>
-            <p>Click here to start the search for your ideal event!</p>
-          </div>
+      <div class="grid gap-20 grid-cols-7 grid-rows-2 mx-16 mt-16 mb-16">
+        <div className="image-container col-span-4">
+          <h2>Looking to find an event?</h2>
+          <p>Click here to start the search for your ideal event!</p>
         </div>
-        <div className="image-container">
+        <div className="image-container col-span-3">
           <Image src={funrun} alt="" />
         </div>
-        <div className="image-container">
+        <div className="image-container col-span-3">
           <Image src={concert} alt="" />
         </div>
-        <div class="col-span-2">
-          <div className="image-container">
-            <h2>Looking to post an event?</h2>
-            <p>Click here to start creating you post!</p>
-          </div>
+        <div className="image-container col-span-4">
+          <h2>Looking to post an event?</h2>
+          <p>Click here to start creating you post!</p>
         </div>
       </div>
     </main>

3adb7fb76c52bb06e949a0137262c50f0290ce44
Frank Barton <frankcbarton@gmail.com>
2023-11-08 19:42:40 -0500

    Fix styling issue on sinin page

--- app/signin/page.jsx
+++ app/signin/page.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React from "react";
 import { Fragment } from "react";
-import './SignIn.css';
+import "./SignIn.css";
 
 export default function SignIn() {
   return (
@@ -11,12 +11,21 @@ export default function SignIn() {
           <form className="sign-in-form">
             <input type="email" placeholder="Email" required />
             <input type="password" placeholder="Password" required />
-            <button type="submit" className="bg-yellow-200 rounded hover:bg-yellow-300 hover:text-black transition duration-300">Sign In</button>
+            <button
+              type="submit"
+              className="bg-yellow-200 rounded hover:bg-yellow-300 hover:text-black transition duration-300"
+            >
+              Sign In
+            </button>
           </form>
         </div>
         <div className="sign-up-container">
           <h2 className="sign-up-question">{"Don't have an account?"}</h2>
-          <a href="/signup"><button>Create Account</button></a>
+          <a href="/signup">
+            <button className="bg-yellow-200 rounded hover:bg-yellow-300 hover:text-black transition duration-300">
+              Create Account
+            </button>
+          </a>
         </div>
       </div>
     </Fragment>

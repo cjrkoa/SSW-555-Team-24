911e3dd782e67f64cab9a3dff3faedfc20fe201f
Samuelp0110 <spreston110@gmail.com>
2023-11-16 19:22:36 -0500

    footer update
    Added page templates for footer links as well as proper routing for said links

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,46 +1,54 @@
 const LINKS = [
-    {
-      title: "Connect",
-      items: ["Help", "Inquire", "Liscensing"]
-    },
-    {
-      title: "Finding Events",
-      items: ["Recommendations", "Searching"]
-    },
-    {
-      title: "Creating Events",
-      items: ["Demonstration", "Limitations"]
-    }
-  ];
-  
-  const currentYear = new Date().getFullYear();
-  
-  const Footer = () => {
-    return (
-      <footer className="footer">
-        <div className="content">
-          <h5>SHAREVENT</h5>
-          <div className="links">
-            {LINKS.map(({ title, items }) => (
-              <ul key={title}>
-                <li className="title">{title}</li>
-                {items.map((link) => (
-                  <li key={link}>
-                    <a href="#" className="link">{link}</a>
-                  </li>
-                ))}
-              </ul>
-            ))}
-          </div>
-        </div>
-        <div className="footer-bottom">
-          <p>
-            &copy; {currentYear} <a href="#">Sharevent</a>. All Rights Reserved.
-          </p>
-          {/* Social icons can be added here */}
-        </div>
-      </footer>
-    );
+  {
+    title: "Connect",
+    items: [
+      { name: "Help", url: "/footerconnect/help" },
+      { name: "Inquire", url: "/footerconnect/inquire" },
+      { name: "Licensing", url: "/footerconnect/licensing" }
+    ]
+  },
+  {
+    title: "Finding Events",
+    items: [
+      { name: "Recommendations", url: "/footerfind/recommendations" },
+      { name: "Searching", url: "/footerfind/searching" }
+    ]
+  },
+  {
+    title: "Creating Events",
+    items: [
+      { name: "Demonstration", url: "/footercreate/demonstration" },
+      { name: "Limitations", url: "/footercreate/limitations" }
+    ]
   }
-  
-  export default Footer
+];
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="content">
+        <h5>SHAREVENT</h5>
+        <div className="links">
+          {LINKS.map(({ title, items }) => (
+            <ul key={title}>
+              <li className="title">{title}</li>
+              {items.map(({ name, url }) => (
+                <li key={name}>
+                  <a href={url} className="link">{name}</a>
+                </li>
+              ))}
+            </ul>
+          ))}
+        </div>
+      </div>
+      <div className="footer-bottom">
+        <p>
+          &copy; {new Date().getFullYear()} <a href="#">Sharevent</a>. All Rights Reserved.
+        </p>
+        {/* Social icons can be added here */}
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;

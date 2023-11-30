54ee155622d018e7f119aa5def354b23308da15e
Christopher Roddy <76755988+cjrkoa@users.noreply.github.com>
2023-11-30 12:35:55 -0500

    update inputs of SignIn

--- app/signin/page.jsx
+++ app/signin/page.jsx
@@ -1,17 +1,45 @@
+"use client"
+
 import React from "react";
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import "../styles/SignIn_SignUp.css";
 
 export default function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  
   return (
     <Fragment>
       <div className="page-container">
         <div className="primary-container">
           <h1 className="primary-heading">Sign In</h1>
           <form className="form">
-            <input type="email" placeholder="Email" required />
-            <input type="password" placeholder="Password" required />
-            <button type="submit">
+            <input 
+                name="email" 
+                type="email"
+                placeholder="Email"
+                onChange={e => setEmail(e.target.value)}  
+                required 
+              />
+            <input 
+                name="password" 
+                type="password"
+                placeholder="Password" 
+                onChange={e => setPassword(e.target.value)} 
+                required 
+              />
+            <button 
+              type="submit"
+              onClick={() => {
+                const user = {
+                  email: email,
+                  password: password,
+                };
+
+                //TODO: replace with user verification when avalible
+                console.log(user);
+              }}
+            >
               Sign In
             </button>
           </form>

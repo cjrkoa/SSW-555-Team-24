f01b372f85b1634d4bd1cd6f9b67a082fad88e01
Christopher Roddy <76755988+cjrkoa@users.noreply.github.com>
2023-11-29 21:01:01 -0500

    Update AddEvent

--- app/createvent/page.jsx
+++ app/createvent/page.jsx
@@ -1,7 +1,7 @@
 "use client"
 
 import React from 'react';
-import { Fragment, useState, useEffect } from "react";
+import { Fragment, useState } from "react";
 
 export const AddEvent = () => {
     const [name, setName] = useState("");
@@ -9,13 +9,12 @@ export const AddEvent = () => {
     const [price, setPrice] = useState(0);
     const [category, setCategory] = useState("");
     const [type, setType] = useState("");
-    const [language, setLanguage] = useState("");
-    const [currency, setCurrency] = useState('');
-    const [isClient, setIsClient] = useState(false);
+    const [language, setLanguage] = useState("English");
+    const [currency, setCurrency] = useState("USD");
 
-    useEffect(() => {
-        setIsClient(true)
-      }, [])
+    const handleChange = (event, setter) => {
+        setter(event.target.value);
+    }
 
     return(
         <Fragment>
@@ -25,33 +24,86 @@ export const AddEvent = () => {
                 <div class="grid gap-3 grid-cols-1 grid-rows-1">
                     <div class="inline-flex gap-2">
                         <h1>Name</h1>
-                        <input name="name" class="bg-stone-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" onChange={(input) => setName(input)} />
+                        <input 
+                            name="name" 
+                            class="bg-stone-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                            value={name}
+                            onChange={e => setName(e.target.value)} 
+                        />
                     </div>
                     <div class="inline-flex gap-2">
                         <h1>Date</h1>
-                        <input name="date" class="bg-stone-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" onChange={() => setDate()}/>
+                        <input 
+                            name="date"
+                            type="date" 
+                            class="bg-stone-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                            value={date}
+                            onChange={e => setDate(e.target.value)}
+                        />
                     </div>
                     <div class="inline-flex gap-2">
                         <h1>Price</h1>
-                        <input name="price" class="bg-stone-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" onChange={() => setPrice()}/>
+                        <input 
+                            name="price"
+                            type="number" 
+                            class="bg-stone-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                            value={price}
+                            onChange={e => setPrice(e.target.value)}
+                        />
                     </div>
                     <div class="inline-flex gap-2">
                         <h1>Category</h1>
-                        <input name="category" class="bg-stone-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" onChange={() => setCategory()}/>
+                        <input 
+                            name="category" 
+                            class="bg-stone-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                            value={category}
+                            onChange={e => setCategory(e.target.value)}
+                        />
                     </div>
                     <div class="inline-flex gap-2">
                         <h1>Type</h1>
-                        <input name="type" class="bg-stone-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" onChange={() => setType()}/>
+                        <input 
+                            name="type" 
+                            class="bg-stone-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                            value={type}
+                            onChange={e => setType(e.target.value)}
+                        />
                     </div>
                     <div class="inline-flex gap-2">
                         <h1>Language</h1>
-                        <input name="language" class="bg-stone-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" onChange={() => setLangauge()}/>
+                        <input 
+                            name="language" 
+                            class="bg-stone-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                            value={language}
+                            onChange={e => setLanguage(e.target.value)}
+                        />
                     </div>
                     <div class="inline-flex gap-2">
                         <h1>Currency</h1>
-                        <input name="currency" class="bg-stone-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" onChange={() => setCurrency()}/>
+                        <input 
+                            name="currency" 
+                            class="bg-stone-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                            value={currency}
+                            onChange={e => setCurrency(e.target.value)}
+                        />
                     </div>
-                        <button class="bg-green-300 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full" onClick={() => console.log(name)}>Submit</button>
+                        <button 
+                            class="bg-green-300 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full" 
+                            onClick={() => {
+                                const event = {
+                                    "name": name,
+                                    "date": date,
+                                    "price": price,
+                                    "category": category,
+                                    "type": type,
+                                    "language": language,
+                                    "currency": currency,
+                                };
+                                //replace with submission to database when avalible
+                                console.log(event);
+                            }}>
+                                Submit
+                        </button>
                 </div>
             </div>
         </Fragment>
